1007b355cd3fa699fdbcc243fc66be2478a092d7
grandhisila.kumar <birendra1.kumar@ril.com>
2024-03-20 09:10:23 +0530

    robodhop scripts modified

---
+++ mongodb.sh
@@ -0,0 +1,28 @@
+#!/bin/bash/
+
+ID=$(id -u)
+
+Timestamp=$(date +%F - %H - %M - $S)
+LOGFILE="/tmp/$0_$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" & >> $LOGFILE
+
+validate () {
+    if [ $? -ne 0 ]
+    then
+       echo "$2 is failed"
+    else
+       echo "$2 is success"
+    fi
+}
+if [ $ID -ne 0 ]
+
+then 
+   echo "Error : Please run this script with root user"
+else 
+   echo "you are root user"
+ fi 
+
+ cp mongo repo /etc/yum.repos/mongo.repo & >> $LOGFILE
+
+ VALIDATE $? "copied mongodb Repo"
