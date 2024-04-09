51537dba37efb9bd16dce90b98688f6be854be7d
grandhisila.kumar <birendra1.kumar@ril.com>
2024-04-09 08:57:30 +0530

    modified

--- deleteold-logs.sh
+++ deleteold-logs.sh
@@ -12,12 +12,3 @@ fi
 
 Files_To_Delete=$(find /tmp/old-logs -type f -mtime +14 -name "*.log")
 
-VALIDATE () {
-    if [ $? -ne 0 ]
-    then
-       echo "*.log fiels not exist in $Source_Directory"
-       exit 1
-    else
-       echo "$Files_To_Delete"
-    fi
-}
