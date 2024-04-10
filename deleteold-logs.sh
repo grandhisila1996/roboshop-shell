6ff6587098c88a2d9c3171a6e5a8914d645baca6
grandhisila.kumar <birendra1.kumar@ril.com>
2024-04-10 12:07:04 +0530

    modified

--- deleteold-logs.sh
+++ deleteold-logs.sh
@@ -9,4 +9,14 @@ else
     echo "Source directory : $Source_Directory is exist"    
 fi
 
-find $Source_Directory -type f -mtime +14 -name "*.log"
+FILES_TO_DELETE=$(find $Source_Directory -type f -mtime +14 -name "*.log")
+
+VALIDATE () {
+    if [ $? -ne 0 ]
+    then
+       echo "*.log fiels not exist in $Source_Directory"
+       exit 1
+    else
+       echo "$Files_To_Delete"
+    fi
+}
