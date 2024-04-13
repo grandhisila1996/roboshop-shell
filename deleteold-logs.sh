2bac72e1751c30c6daca789250307e6e6b00f68d
grandhisila.kumar <birendra1.kumar@ril.com>
2024-04-13 16:42:17 +0530

    modified

--- deleteold-logs.sh
+++ deleteold-logs.sh
@@ -10,11 +10,12 @@ else
     echo "Source directory : $Source_Directory is exist"    
 fi
 
-OLD-LOGS=`$(find $Source_Directory -type f -mtime +14 -name "*.log")`
+OLD-LOGS=$(find "${Source_Directory}" -type f -mtime +"14" -name "*.log")
 echo "$OLD-LOGS"
 while IFS= read -r line
 do 
   echo "Deleting file : $line"
   rm -rf $line
 done <<< $OLD-LOGS
+
 
